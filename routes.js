1a42c844c4fc428a941a926a5b32469dafde268e
Samuel Cabral <samuelcabral.mail@gmail.com>
2020-10-29 21:21:12 -0300

    done: front-end strutucture for Instructor card

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const routes = express.Router();
+const express = require('express')
+const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.redirect('/instructors');
-});
+  return res.redirect('/instructors')
+})
 
 routes.get('/instructors', (req, res) => {
-  return res.render('instructors/index');
-});
+  return res.render('instructors/index')
+})
 
 routes.get('/members', (req, res) => {
-  return res.send('members');
-});
+  return res.send('members')
+})
 
-module.exports = routes;
+module.exports = routes
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
-const routes = require('./routes');
+ 
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
 
-const server = express();
+const server = express()
 
-server.use(express.static('public'));
-server.use(routes);
+server.use(express.static('public'))
+server.use(routes)
 
-server.set('view engine', 'njk');
+server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
   express: server,
   autoescape: false,
   noCache: true,
-});
+})
 
-server.listen(3000, () => {
-  console.log('🚀 server is running on port 3000');
+server.listen(5000, function() {
+  console.log('🚀 server is running on port 5000')
 })
